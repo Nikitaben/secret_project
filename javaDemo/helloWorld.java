29745da4b1dd0f173cfcea7f7080d2c42673954c
Nikitaben <nikitaprajapati88@gmail.com>
2021-06-25 15:06:05 -0600

    "Initialize repository"

---
+++ javaDemo/helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld.java{
+
+      public static void main(String[] args) {
+
+                   
+               System.out.print("Hellow World");
+            
+               }
+}
